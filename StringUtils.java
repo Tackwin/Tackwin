63af301e9ce43a7f1b3fb89c358ec5fc8d513861
Tackwin <lucabrx@hotmail.fr>
2014-12-31 20:23:08 +0100

    StringUtils

--- StringUtils.java
+++ StringUtils.java
@@ -16,4 +16,33 @@ public class StringUtils {
 		return occ;
 	}
 	
+	/**
+	 * Return a String format with multiplier ex:
+	 * formatNumber(10548, 3) = "10.54K" or,
+	 * formatNumber(123456789, 2) = "123.4M"
+	 * @param number
+	 * @param nNumber
+	 * @return
+	 */
+	public static String formatNumber(double number){
+		char n = ' ';
+		char[] mult = {'K', 'M', 'B', 'T', 'q', 'Q', 's', 'S'};
+		for (int i = 0; (float)number/1000 > 1; i++){
+			number /= 1000;
+			n = mult[i];
+		}
+		if (number < 10){
+			number = ((long)(number * 1000));
+			number /= 1000;
+		} else if (number < 100){
+			number = ((long)(number * 100));
+			number /= 100;
+		} else {
+			number = ((long)(number * 10));
+			number /= 10;
+		}
+		String str = Double.toString(number) + Character.toString(n);
+		return str;
+	}
+	
 }
