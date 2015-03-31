f02ebbc053f8dcc80d8b1aa190bbd9876d9db092
Tackwin <lucabrx@hotmail.fr>
2015-03-31 21:32:36 +0200

    Add Window class.

---
+++ Window/Window.java
@@ -0,0 +1,63 @@
+package fr.tackwin.window;
+
+import static org.lwjgl.glfw.Callbacks.errorCallbackPrint;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+
+import org.lwjgl.Sys;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GLContext;
+
+public class Window{
+
+	private String title;
+	
+	private int width;
+	private int height;
+	private long window;
+	
+	private GLFWErrorCallback errorCall = errorCallbackPrint();
+	
+	public Window(String title, int width, int height){
+		this.title = title;
+		
+		this.width = width;
+		this.height = height;
+		
+		System.out.println("Lwjgl: " + Sys.getVersion() + "\nGLFW : " + glfwGetVersionString());
+	}
+	
+	public void init(){
+		glfwSetErrorCallback(errorCall);
+		Input.create();
+		
+		if (glfwInit() != GL_TRUE){
+			System.err.println("Something went wrong with the initialisation of glfw (Window.java : init())");
+			throw(new IllegalStateException());
+		}
+		
+		glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+		
+		window = glfwCreateWindow(width, height, title, 0L, 0L);
+		if (window == 0){
+			System.err.println("Something went wrong with the cretion of window (Window.java : init())");
+			throw(new IllegalStateException());
+		}
+		
+		glfwSetKeyCallback(window, Input.INPUT);
+		glfwMakeContextCurrent(window);
+		glfwShowWindow(window);
+		
+		GLContext.createFromCurrent();
+		glClearColor(0, 0, 0, 1);
+	}
+	
+	public int getWidth(){
+		return width;
+	} public int getHeight(){
+		return height;
+	} public long get(){
+		return window;
+	}
+	
+}
